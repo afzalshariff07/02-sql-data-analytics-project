3a6d7fb3a021cde71d9a782d201722e5c68b3a8e
Mohammed Afzal Shariff <afzalshariff@gmail.com>
2026-01-29 13:55:23 +0530

    Added 07_change_over_time_analysis.sql file

---
+++ scripts/07_change_over_time_analysis.sql
@@ -0,0 +1,77 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+USE DataWarehouse;
+GO
+
+-- Analyse sales performance over time
+-- Quick Date Functions
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY 
+    YEAR(order_date),
+    MONTH(order_date)
+ORDER BY 
+    YEAR(order_date),
+    MONTH(order_date);
+
+
+-- DATETRUNC()
+SELECT
+    DATETRUNC(MONTH,order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH,order_date)
+ORDER BY DATETRUNC(MONTH,order_date);
+
+-- FORMAT()
+SELECT
+    FORMAT(order_date,'MMM-yyyy') AS order_month,
+    DATETRUNC(MONTH, order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_orders
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date,'MMM-yyyy'),
+         DATETRUNC(MONTH, order_date)
+ORDER BY DATETRUNC(MONTH, order_date),
+         FORMAT(order_date,'MMM-yyyy');
+
+
+-- My CUSTOMIZED FORMAT()
+SELECT
+    FORMAT(order_date, 'MMM-yyyy') AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY 
+    YEAR(order_date),
+    MONTH(order_date),
+    FORMAT(order_date, 'MMM-yyyy')
+ORDER BY 
+    YEAR(order_date),
+    MONTH(order_date);
+
