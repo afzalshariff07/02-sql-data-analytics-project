f9ea9b20d18ebf40997827dfcbeb9479f8631b3f
Mohammed Afzal Shariff <afzalshariff@gmail.com>
2026-01-30 23:02:11 +0530

    Added 10_part_to_whole_analysis.sql file

---
+++ scripts/10_part_to_whole_analysis.sql
@@ -0,0 +1,20 @@
+/*
+===============================================================================
+Part-to-Whole Analysis
+===============================================================================
+Purpose:
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
+
+SQL Functions Used:
+    - SUM(), AVG(): Aggregates values for comparison.
+    - Window Functions: SUM() OVER() for total calculations.
+===============================================================================
+*/
+
+USE DataWarehouse;
+GO
+
+-- Which categories contribute the most to overall sales?
+
